fb4fd74ee570981045dbc63920199bc9e264816b
Sage Vilaga <itshurtsmans@gmail.com>
2024-11-13 01:43:22 +0800

    finalized add-dog component, made some small visual tweaks to edit-dog

--- src/app/components/add-dog/add-dog.component.ts
+++ src/app/components/add-dog/add-dog.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Dog } from '../../models/dog.model';
 
 @Component({
   selector: 'app-add-dog',
   templateUrl: './add-dog.component.html',
-  styleUrl: './add-dog.component.css'
+  styleUrls: ['./add-dog.component.css']
 })
-export class AddDogComponent {
+export class AddDogComponent implements OnInit {
+  addForm!: FormGroup;
 
+  constructor(
+    private http: HttpClient,
+    private fb: FormBuilder,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  initializeForm(): void {
+    this.addForm = this.fb.group({
+      name: ['', Validators.required],
+      breed: ['', Validators.required],
+      age: [0, [Validators.required, Validators.min(0)]],
+      description: ['', Validators.required],
+      available: [true]
+    });
+  }
+
+  onSubmit(): void {
+    if (this.addForm.valid) {
+      // Prepare the form data for submission
+      const newDog: Dog = this.addForm.value;
+
+      // Send a POST request to add the new dog
+      this.http.post('http://localhost:18080/api/add-dog', newDog).subscribe({
+        next: () => {
+          console.log('Dog added successfully');
+          // Redirect to the dashboard after saving
+          this.router.navigate(['/admin']);
+        },
+        error: (err) => console.error('Error adding dog', err)
+      });
+    }
+  }
+
+  goBack(): void {
+    this.router.navigate(['/admin']);
+  }
 }
